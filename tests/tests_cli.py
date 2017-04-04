8731bba2c33b659fcb1ace5178cb56c967e87889
Marco Buttu <marco.buttu@gmail.com>
2017-04-04 12:39:01 +0200

    Change from discos_station to large or small
    Ex: discos_srt:development  -> large:development

--- tests/tests_cli.py
+++ tests/tests_cli.py
@@ -12,14 +12,14 @@ class TestCLI(unittest.TestCase):
 
     def test_cannot_split_the_system_in_cluster_and_env(self):
         """Cluster and env are not separated by :"""
-        cmd = BASECMD + ['discos_srt_development']
+        cmd = BASECMD + ['large_development']
         pipes = Popen(cmd, stdout=PIPE, stderr=PIPE)
         out, err = pipes.communicate()
         self.assertRegexpMatches(err, b'You must specify an available system.')
 
     def test_too_much_separators(self):
         """Too much : separators"""
-        cmd = BASECMD + ['discos_srt:development:srt']
+        cmd = BASECMD + ['large:development:srt']
         pipes = Popen(cmd, stdout=PIPE, stderr=PIPE)
         out, err = pipes.communicate()
         self.assertRegexpMatches(err, b'You must specify an available system.')
@@ -33,7 +33,7 @@ class TestCLI(unittest.TestCase):
 
     def test_right_cluster_and_wrong_env(self):
         """The user specify a right_cluster and wrong_env"""
-        cmd = BASECMD + ['discos_srt:wrong_env']
+        cmd = BASECMD + ['large:wrong_env']
         pipes = Popen(cmd, stdout=PIPE, stderr=PIPE)
         out, err = pipes.communicate()
         self.assertRegexpMatches(err, b'"wrong_env" not found')
@@ -47,7 +47,7 @@ class TestCLI(unittest.TestCase):
 
     def test_no_deploy(self):
         """Do not set the tag 'deploy'"""
-        cmd = BASECMD + ['discos_noto:development']
+        cmd = BASECMD + ['small:development']
         pipes = Popen(cmd, stdout=PIPE, stderr=PIPE)
         out, err = pipes.communicate()
         self.assertNotRegexpMatches(out, b'branch')
@@ -55,7 +55,7 @@ class TestCLI(unittest.TestCase):
 
     def test_deploy_branch(self):
         """Set the tag 'deploy'"""
-        cmd = BASECMD + ['discos_srt:development', '--deploy', 'srt-0.1']
+        cmd = BASECMD + ['large:development', '--deploy', 'srt-0.1']
         pipes = Popen(cmd, stdout=PIPE, stderr=PIPE)
         out, err = pipes.communicate()
         self.assertRegexpMatches(out, b'branch=srt-0.1')
@@ -63,7 +63,7 @@ class TestCLI(unittest.TestCase):
 
     def test_deploy_master_no_station(self):
         """Require the station in case of master branch"""
-        cmd = BASECMD + ['discos_srt:development', '--deploy', 'master']
+        cmd = BASECMD + ['large:development', '--deploy', 'master']
         pipes = Popen(cmd, stdout=PIPE, stderr=PIPE)
         out, err = pipes.communicate()
         self.assertRegexpMatches(err, b'--station is required')
@@ -71,7 +71,7 @@ class TestCLI(unittest.TestCase):
     def test_deploy_master(self):
         """Set the station"""
         cmd = BASECMD + [
-            'discos_noto:development',
+            'small:development',
             '--deploy',
             'master',
             '--station', 'srt']
@@ -82,7 +82,7 @@ class TestCLI(unittest.TestCase):
     def test_only_master_accepts_station(self):
         """You can not set the -s if the branch is not master."""
         cmd = BASECMD + [
-            'discos_srt:development',
+            'large:development',
             '--deploy',
             'srt-0.1',
             '--station',
@@ -94,7 +94,7 @@ class TestCLI(unittest.TestCase):
     def test_deploy_github_username(self):
         """Pass the GitHub username from command line"""
         cmd = BASECMD + [
-            'discos_noto:development',
+            'small:development',
             '--deploy',
             'srt-0.1',
             '--user', 'pippo']
@@ -105,7 +105,7 @@ class TestCLI(unittest.TestCase):
     def test_deploy_github_username_requires_deploy(self):
         """Pass the GitHub username from command line"""
         cmd = BASECMD + [
-            'discos_noto:development',
+            'small:development',
             '--user', 'pippo']
         pipes = Popen(cmd, stdout=PIPE, stderr=PIPE)
         out, err = pipes.communicate()
