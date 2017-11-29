ff9b3cb7e7c933357c8075f189a6ef2c2f635ecd
Giuseppe Carboni <giuseppecarboni89@live.com>
2017-11-29 10:35:09 +0100

    Fix #35. 'discos' user scripts moved to '.bin/'

--- ansible/roles/common/templates/prompt_command.sh
+++ ansible/roles/common/templates/prompt_command.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-CHECK_OUTPUT=$(/usr/bin/_discos-check-branch)
+CHECK_OUTPUT=$($HOME/.bin/_discos-check-branch)
 if [ -n "${CHECK_OUTPUT}" ]
 then
     echo -e "${pur}WARNING:${txtrst} $CHECK_OUTPUT"
