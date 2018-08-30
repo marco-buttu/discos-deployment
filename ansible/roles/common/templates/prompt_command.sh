9d035f82b26cc63ccff8da61b3bfd33d77141271
Giuseppe Carboni <giuseppecarboni89@live.com>
2018-08-30 18:43:45 +0200

    Fix #124, grant access to the introot to observer
    A major rework of the whole process has been made in order to create the
    introot outside the `discos` user home directory. Access to the introot
    is now granted with read and execute permissions to all acs group
    members. This will enable a user `observer`, which should be created in
    console machine, to launch the discos console.

--- ansible/roles/common/templates/prompt_command.sh
+++ ansible/roles/common/templates/prompt_command.sh
@@ -1,9 +1,10 @@
 #! /usr/bin/env bash
 
-CHECK_OUTPUT=$($HOME/bin/_discos-check-branch)
-if [ -n "${CHECK_OUTPUT}" ]
-then
-    echo -e "${pur}WARNING:${txtrst} $CHECK_OUTPUT"
+if [ -f $HOME/bin/_discos-check-branch ]; then
+	CHECK_OUTPUT=$($HOME/bin/_discos-check-branch)
+	if [ -n "${CHECK_OUTPUT}" ]; then
+		echo -e "${pur}WARNING:${txtrst} $CHECK_OUTPUT"
+	fi
 fi
 
 source ~/.bashrc
