740b77f6a197774236e4985b7faf70cccebecd84
Giuseppe Carboni <giuseppecarboni89@live.com>
2019-03-07 16:53:28 +0100

    Fix #287, updated quicklook configuration (#288)

--- ansible/roles/quicklook/templates/service.sh
+++ ansible/roles/quicklook/templates/service.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /{{ discos_sw_dir}}/sdtools/quicklook/page
-exec /usr/bin/SDTmonitor -c ../conf/monitor_config.ini --polling --nosave --http-server-port {{ quicklook_server_port }} {{ quicklook_directories | join(' ') }}
+exec /usr/bin/SDTmonitor -c ../conf/monitor_config.ini --polling --nosave -w {{ quicklook_workers }} --http-server-port {{ quicklook_server_port }} {{ quicklook_directories | join(' ') }}
